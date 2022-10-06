b66b530f71ae97377d3cfce6cc9cf36072e066d0
Odubo Tari <jerryebikarineedam@gmail.com>
2022-10-06 17:23:47 +0100

    Project boilerplate setup (Images included)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="images/logo.svg" alt="" />
+    </div>
+  );
+}
+
+export default App;
